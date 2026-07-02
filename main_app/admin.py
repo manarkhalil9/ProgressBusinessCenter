bd23acad2dc8fdf2ccb6506832fb0f322ae2b098
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-07-02 13:37:31 +0300

    URLs done

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Service
+from .models import (Service, Feature, Branch, MeetingRoom, Event, GalleryImage, FAQ, ContactMessage, VisitRequest, BusinessRegistration, Referral)
 
 # Register your models here.
 admin.site.register(Service)
+admin.site.register(Feature)
+admin.site.register(Branch)
+admin.site.register(MeetingRoom)
+admin.site.register(Event)
+admin.site.register(GalleryImage)
+admin.site.register(FAQ)
+admin.site.register(ContactMessage)
+admin.site.register(VisitRequest)
+admin.site.register(BusinessRegistration)
+admin.site.register(Referral)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Service
+from django.shortcuts import render, redirect, get_list_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import (Service, Feature, Branch, MeetingRoom, Event, GalleryImage, FAQ, ContactMessage, VisitRequest, BusinessRegistration, Referral)
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -13,14 +14,82 @@ def about(request):
     return render(request, 'about.html')
 
 #services
-
-#list services 
+#list 
 class ServiceList(ListView):
     model = Service
     template_name = 'services/index.html'
     context_object_name = 'services'
 
+# detail
 class ServiceDetail(DetailView):
     model = Service
     template_name = 'services/detail.html'
     context_object_name = 'service'
+
+# create
+class ServiceCreateView(CreateView):
+    model = Service
+    fields = '__all__'
+    success_url = reverse_lazy('services')
+
+# update
+class ServiceUpdateView(UpdateView):
+    model = Service
+    fields = '__all__'
+    success_url = reverse_lazy('services')
+
+# delete
+class ServiceDeleteView(DeleteView):
+    model = Service
+    success_url = reverse_lazy('services')
+
+# features
+# list
+class FeatureListView(ListView):
+    model = Feature
+    template_name = 'features/index.html'
+
+# detail
+class FeatureDetailView(DetailView):
+    model = Feature
+    template_name = 'features/detail.html'
+
+# create
+class FeatureCreateView(CreateView):
+    model = Feature
+    fields = '__all__'
+    success_url = reverse_lazy('features')
+
+# update
+class FeatureUpdateView(UpdateView):
+    model = Feature
+    fields = '__all__'
+    success_url = reverse_lazy('features')
+
+# delete
+class FeatureDeleteView(DeleteView):
+    model = Feature
+    success_url = reverse_lazy('features')
+
+# branches
+class BranchListView(ListView):
+    model = Branch
+    template_name = 'branches/index.html'
+
+class BranchDetailView(DetailView):
+    model = Branch
+    template_name = 'branches/detail.html'
+
+class BranchCreateView(CreateView):
+    model = Branch
+    fields = '__all__'
+    success_url = reverse_lazy('branches')
+
+class BranchUpdateView(UpdateView):
+    model = Branch
+    fields = '__all__'
+    success_url = reverse_lazy('branches')
+
+class BranchDeleteView(DeleteView):
+    model = Branch
+    success_url = reverse_lazy('branches')
