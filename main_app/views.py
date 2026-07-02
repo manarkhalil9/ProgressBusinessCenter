1aab0452e1647e8510a6dd7da64bc3b946c59b5c
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-07-02 14:28:17 +0300

    views are done & business index template

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import (Service, Feature, Branch, MeetingRoom, Event, GalleryImage, FAQ, ContactMessage, VisitRequest, BusinessRegistration, Referral)
 from django.urls import reverse_lazy
@@ -72,24 +72,147 @@ class FeatureDeleteView(DeleteView):
     success_url = reverse_lazy('features')
 
 # branches
+# list
 class BranchListView(ListView):
     model = Branch
     template_name = 'branches/index.html'
 
+# detail
 class BranchDetailView(DetailView):
     model = Branch
     template_name = 'branches/detail.html'
 
+# create
 class BranchCreateView(CreateView):
     model = Branch
     fields = '__all__'
     success_url = reverse_lazy('branches')
 
+# update
 class BranchUpdateView(UpdateView):
     model = Branch
     fields = '__all__'
     success_url = reverse_lazy('branches')
 
+# delete
 class BranchDeleteView(DeleteView):
     model = Branch
     success_url = reverse_lazy('branches')
+
+# meeting rooms
+# list
+class MeetingRoomListView(ListView):
+    model = MeetingRoom
+    template_name = 'rooms/index.html'
+
+# detail
+class MeetingRoomDetailView(DetailView):
+    model = MeetingRoom
+    template_name = 'rooms/detail.html'
+
+# create
+class MeetingRoomCreateView(CreateView):
+    model = MeetingRoom
+    fields = '__all__'
+    success_url = reverse_lazy('rooms')
+
+# update
+class MeetingRoomUpdateView(UpdateView):
+    model = MeetingRoom
+    fields = '__all__'
+    success_url = reverse_lazy('rooms')
+
+# delete
+class MeetingRoomDeleteView(DeleteView):
+    model = MeetingRoom
+    success_url = reverse_lazy('rooms')
+
+# events
+# list
+class EventListView(ListView):
+    model = Event
+    template_name = 'events/index.html'
+
+# detail
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'events/detail.html'
+
+# create
+class EventCreateView(CreateView):
+    model = Event
+    fields = '__all__'
+    success_url = reverse_lazy('events')
+
+# update
+class EventUpdateView(UpdateView):
+    model = Event
+    fields = '__all__'
+    success_url = reverse_lazy('events')
+
+# delete
+class EventDeleteView(DeleteView):
+    model = Event
+    success_url = reverse_lazy('events')
+
+# gallery
+# list
+class GalleryListView(ListView):
+    model = GalleryImage
+    template_name = 'gallery/index.html'
+
+# detail
+class GalleryDetailView(DetailView):
+    model = GalleryImage
+    template_name = 'gallery/detail.html'
+
+# FAQ
+# list
+class FAQListView(ListView):
+    model = FAQ
+    template_name = 'faqs/index.html'
+
+# detail
+class FAQDetailView(DetailView):
+    model = FAQ
+    template_name = 'faqs/detail.html'
+
+# contact, visit, referral
+# create
+class ContactCreateView(CreateView):
+    model = ContactMessage
+    fields = '__all__'
+    success_url = reverse_lazy('home')
+
+class VisitCreateView(CreateView):
+    model = VisitRequest
+    fields = '__all__'
+    success_url = reverse_lazy('home')
+
+class ReferralCreateView(CreateView):
+    model = Referral
+    fields = '__all__'
+    success_url = reverse_lazy('home')
+
+# business
+# list
+class BusinessListView(ListView):
+    model = BusinessRegistration
+    template_name = 'business/index.html'
+
+# detail
+class BusinessDetailView(DetailView):
+    model = BusinessRegistration
+    template_name = 'business/detail.html'
+
+# many to many 'business - services'
+def add_service(request, business_id, service_id):
+    business = get_object_or_404(BusinessRegistration, id=business_id)
+    business.services.add(service_id)
+    return redirect('business_detail', pk=business_id)
+
+
+def remove_service(request, business_id, service_id):
+    business = get_object_or_404(BusinessRegistration, id=business_id)
+    business.services.remove(service_id)
+    return redirect('business_detail', pk=business_id)
